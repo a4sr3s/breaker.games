7bfb971a2485b6ddbebff003bfdab1fb88370140
Alexis Reyes <areyesjr@users.noreply.github.com>
2024-04-06 22:13:00 -0400

    mobile friendly

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        '8xl': '7rem', // Add custom size as needed
+        '3xl': '2rem',  // Ensure this aligns with your design requirements
+      },
+    },
+  },
+}
